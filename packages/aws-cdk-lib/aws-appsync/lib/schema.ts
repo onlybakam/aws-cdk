618464c84f1bbf4d1227b29119cd4e68cc808df3
Brice Pellé <onlybakam@gmail.com>
2023-08-06 16:01:56 -0700

    allow to define a schema backed by an S3 location

--- packages/aws-cdk-lib/aws-appsync/lib/schema.ts
+++ packages/aws-cdk-lib/aws-appsync/lib/schema.ts
@@ -1,21 +1,28 @@
 import { readFileSync } from 'fs';
 import { IGraphqlApi } from './graphqlapi-base';
+import * as s3_assets from '../../aws-s3-assets';
+import * as cdk from '../../core';
 
 /**
-  * Configuration for bound graphql schema
-  *
-  * Returned from ISchema.bind allowing late binding of schemas to graphqlapi-base
-  */
+ * Configuration for bound graphql schema
+ *
+ * Returned from ISchema.bind allowing late binding of schemas to graphqlapi-base
+ */
 export interface ISchemaConfig {
   /**
-    * The ID of the api the schema is bound to
-    */
+   * The ID of the api the schema is bound to
+   */
   apiId: string;
 
   /**
-    * The schema definition string
-    */
-  definition: string;
+   * The schema definition string
+   */
+  definition?: string;
+
+  /**
+   * The location of the schema file in S3 (mutually exclusive with `definition`).
+   */
+  definitionS3Location?: string;
 }
 
 /**
@@ -27,18 +34,18 @@ export interface ISchemaConfig {
 export interface SchemaBindOptions {}
 
 /**
-  * Interface for implementing your own schema
-  *
-  * Useful for providing schema's from sources other than assets
-  */
+ * Interface for implementing your own schema
+ *
+ * Useful for providing schema's from sources other than assets
+ */
 export interface ISchema {
   /**
-    * Binds a schema string to a GraphQlApi
-    *
-    * @returns ISchemaConfig with apiId and schema definition string
-    * @param api the api to bind the schema to
-    * @param options configuration for bind behavior
-    */
+   * Binds a schema string to a GraphQlApi
+   *
+   * @returns ISchemaConfig with apiId and schema definition string
+   * @param api the api to bind the schema to
+   * @param options configuration for bind behavior
+   */
   bind(api: IGraphqlApi, options?: SchemaBindOptions): ISchemaConfig;
 }
 
@@ -51,8 +58,8 @@ export interface SchemaProps {
    * configured, then the schema will be generated from an
    * existing file from disk.
    */
-  readonly filePath: string,
-};
+  readonly filePath: string;
+}
 
 /**
  * The Schema for a GraphQL Api
@@ -90,6 +97,39 @@ export class SchemaFile implements ISchema {
     return {
       apiId: api.apiId,
       definition: this.definition,
+    };
+  }
+}
+
+/**
+ * The Schema for a GraphQL Api backed by an S3 location
+ */
+export class SchemaAsset implements ISchema {
+  /**
+   * Generate a Schema from file
+   *
+   * @returns `SchemaAsset` with immutable schema defintion
+   * @param path the file path of the schema file
+   */
+  public static fromAsset(path: string, options?: s3_assets.AssetOptions): SchemaAsset {
+    return new SchemaAsset(path, options);
+  }
+
+  private asset?: s3_assets.Asset;
+
+  public constructor(public readonly path: string, private readonly options: s3_assets.AssetOptions = {}) {}
+
+  public bind(api: IGraphqlApi, _options?: SchemaBindOptions): ISchemaConfig {
+    // If the same AssetCode is used multiple times, retain only the first instantiation.
+    if (!this.asset) {
+      this.asset = new s3_assets.Asset(api, 'schema', { path: this.path, ...this.options });
+    } else if (cdk.Stack.of(this.asset) !== cdk.Stack.of(api)) {
+      throw new Error(`Asset is already associated with another stack '${cdk.Stack.of(this.asset).stackName}'. ` + 'Create a new schema instance for every stack.');
+    }
+
+    return {
+      apiId: api.apiId,
+      definitionS3Location: this.asset.s3ObjectUrl,
     };
   }
 }
